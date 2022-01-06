3775eda9cc73fc8303907f3297a41744455006bb
CrissDg <60599969+Cristian-Dg@users.noreply.github.com>
2022-01-06 15:30:40 +0100

    Add files via upload

---
+++ bbdd/configuracion.php
@@ -0,0 +1,7 @@
+<?php
+	define ("HOST","localhost");
+	define ("DBNAME","listadoanime");
+	define ("USER","animeroot");
+	define ("PASS","Abc123.");
+	define ("numOfertas",3);
+?>
---
+++ inc/pie.php
@@ -0,0 +1,9 @@
+	<footer class="footer">
+	  <p><strong>©DreamTeam</strong> -<i>2022</i>-</p>
+	</footer>
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+	<script src="js/scripts.js"></script>
+</body>
+
+</html>
