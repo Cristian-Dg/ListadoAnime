35e9b5c5281747f61c523456925958f1173617cd
CrissDg <60599969+Cristian-Dg@users.noreply.github.com>
2022-01-06 21:46:44 +0100

    Delete listadoAnime directory

--- listadoAnime/bbdd/configuracion.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define ("HOST","localhost");
-	define ("DBNAME","listadoanime");
-	define ("USER","animeroot");
-	define ("PASS","Abc123.");
-	define ("numOfertas",3);
-?>
--- listadoAnime/inc/pie.php
+++
@@ -1,9 +0,0 @@
-	<footer class="footer">
-	  <p><strong>©DreamTeam</strong> -<i>2022</i>-</p>
-	</footer>
-	
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-	<script src="js/scripts.js"></script>
-</body>
-
-</html>
